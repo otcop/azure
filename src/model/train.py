237da1184fa438aa8c65d4e87da38518b8bdd191
otcop <118574711+otcop@users.noreply.github.com>
2023-07-18 14:35:39 -0500

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -12,8 +12,10 @@ from sklearn.model_selection import train_test_split
 
 
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness',
-               'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose',
+               'DiastolicBloodPressure', 'TricepsThickness',
+               'SerumInsulin', 'BMI', 'DiabetesPedigree',
+               'Age']].values, df['Diabetic'].values
     return train_test_split(X, y, test_size=0.30, random_state=0)
 
 
